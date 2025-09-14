a8d1c20dad165d042087bce5b3f41f10498a72e9
Josh Justice <me@codingitwrong.com>
2025-09-14 11:13:59 -0400

    Update to Expo 54

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,13 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const {getDefaultConfig} = require('expo/metro-config');
-const defaultSourceExts =
-  require('metro-config/src/defaults/defaults').sourceExts;
-const baseSourceExts = [...defaultSourceExts, 'cjs'];
+
+const config = getDefaultConfig(__dirname);
+const baseSourceExts = [...config.resolver.sourceExts, 'cjs'];
 
 module.exports = {
-  ...getDefaultConfig(__dirname),
+  ...config,
   resolver: {
+    ...config.resolver,
     sourceExts:
       process.env.MOCK_API === 'true'
         ? ['mock.js', ...baseSourceExts]
